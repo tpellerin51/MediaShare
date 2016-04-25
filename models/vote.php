b8427011cb8b9833e3cb98586e502f7e844a7657
dgrobs13 <devinrobson@Devins-MacBook-Pro.local>
2016-04-25 12:33:52 -0400

    Voting
    work in progress

--- models/vote.php
+++ models/vote.php
@@ -1,58 +1,39 @@
 <?php
 
-var post_ID;
-var voteDownScript='voteDown.php';
-var voteUpScript='voteUp.php';
+class Vote {
+    private $db;
+    
+    function __construct($db){
+        $this->db = $db;
+    }
+    
+    // Safely acquire the rows
+    function select($query){
+        $select = $this->db->prepare($query);
+        $select->bindParam('username', $this->username, PDO::PARAM_STR);
+        $select->bindParam('title', $this->title, PDO::PARAM_STR);
+        $select->bindParam('post', $this->post, PDO::PARAM_STR);
+        $select->bindParam('upvote', $this->upvote, PDO::PARAM_INT);
+        $select->bindParam('downvote', $this->downvote, PDO::PARAM_INT);
+        $select->bindParam('post_ID', $this->post_ID, PDO::PARAM_INT);
+        $select->execute();
+        return $select->fetchAll(PDO::FETCH_ASSOC);
+    }
+	
+	function UpVote($username, $up_vote, $post_ID){
+		$up_vote++;
+        $insert->bindParam(':username', $username, PDO::PARAM_STR);
+        $insert->bindParam(':up_vote', $up_vote, PDO::PARAM_STR);
+        $insert->bindParam(':post_ID', $post_ID, PDO::PARAM_ID);
+        return $insert->execute();
+    }
+	
+	function DownVote($username, $down_vote, $post_ID){
+		$down_vote--;
+        $insert->bindParam(':username', $username, PDO::PARAM_STR);
+        $insert->bindParam(':down_vote', $down_vote, PDO::PARAM_STR);
+        $insert->bindParam(':post_ID', $post_ID, PDO::PARAM_ID);
+        return $insert->execute();
+    }
 
-// vote up
-$("button.up").click(function(){ // when people click an up button
-	$("div#response").show().html('<h2>voting, please wait...</h2>'); // show wait message
-
-	itemID=$(this).val(); // get post id
-
-	$.post(voteUpScript,{id:itemID},function(response){ // post to up script
-		$("div#response").html(response).hide(3000); // show response
-	});
-
-	$(this).attr({"disabled":"disabled"}); // disable button
- });
-
- // vote down
- $("button.down").click(function(){
-	$("div#response").show().html('<h2>voting, please wait...</h2>');
-
-	itemID=$(this).val();
-	$.post(voteDownScript,{id:itemID},function(response){ // post to down script
-		$("div#response").html(response).hide(3000); // show response
-	});
-	$(this).attr({"disabled":"disabled"}); // disable button
-
- });
- 
- 
-?>
-
-
-
-<button value="1" class="up">UpVote</button>
-		<button value="1" class="down">DownVote</button>
-        	<fieldset>
-                <h2>Post 1</h2>
-                    HI
-                    <p>
-                        <button value="1" class="up">UpVote</button>
-                        <button value="1" class="down">DownVote</button>
-                    </p>
-            </fieldset>
-            
-    <div id="response">
-	</div>
-            
-     
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-    <script type="text/javascript">
-    	$(document).ready(function(){
-    		// all jquery code will go here
-    	});
-</script>
-
+}
